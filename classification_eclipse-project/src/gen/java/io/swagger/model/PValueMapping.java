a2ad57ec5ce5f4558da85224bef9113ec793eac3
Jonathan Alvarsson <jonathan.alvarsson@gmail.com>
2017-10-12 16:34:05 +0200

    generate json

--- classification_eclipse-project/src/gen/java/io/swagger/model/PValueMapping.java
+++ classification_eclipse-project/src/gen/java/io/swagger/model/PValueMapping.java
@@ -20,6 +20,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.*;
 
+import org.json.simple.JSONObject;
+
 /**
  * Mapping of label to predicted p-value
  */
@@ -98,27 +100,15 @@ public class PValueMapping   {
 		return Objects.hash(label, pValue);
 	}
 
-
+	@SuppressWarnings("unchecked")
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class PValueMapping {\n");
+		
+		JSONObject jsonResponse = new JSONObject();
 
-		sb.append("    label: ").append(toIndentedString(label)).append("\n");
-		sb.append("    pValue: ").append(toIndentedString(pValue)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
+		jsonResponse.put("label", getLabel());
+		jsonResponse.put("pValue", getPValue());
 
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces
-	 * (except the first line).
-	 */
-	private String toIndentedString(java.lang.Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
+		return jsonResponse.toJSONString();
 	}
 }
-
