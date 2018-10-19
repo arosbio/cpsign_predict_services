bba410229439f5dbe037adc0b71e996feb14a012
staffan <staffan.arvidsson@gmail.com>
2018-10-19 09:04:22 +0200

    update API for regression - allow empty molecules sent

--- cp-regression_eclipse-project/src/gen/java/io/swagger/api/PredictApi.java
+++ cp-regression_eclipse-project/src/gen/java/io/swagger/api/PredictApi.java
@@ -68,12 +68,16 @@ public class PredictApi  {
 
 			@ApiResponse(code = 503, message = "Service not available", response = Error.class) })
 	public Response predictGet(
-			@ApiParam(value = "(Depricated) Compound structure notation using SMILES notation", required=false)
+			
+			@ApiParam(value = "Compound structure notation using SMILES or MDL format", required=false, defaultValue="CCCCC=O")
+			@QueryParam("molecule") String molecule,
+			
+			@ApiParam(value = "**(Depricated)** Compound structure notation using SMILES notation", required=false)
 			@QueryParam("smiles") String smiles,
-			@ApiParam(value = "Compound structure notation using SMILES or MDL format", required=false)
-			@DefaultValue("CCCCC=O") @QueryParam("molecule") String molecule,
+			
 			@ApiParam(value = "The desired confidence of the prediction", allowableValues="range(0,1)")
 			@DefaultValue("0.8") @QueryParam("confidence") double confidence,
+			
 			@Context SecurityContext securityContext)
 					throws NotFoundException {
 		if (smiles!=null && !smiles.isEmpty()) // TODO - remove in newer versions!
@@ -86,7 +90,7 @@ public class PredictApi  {
 	@Path("/predictImage")
 	@GET
 	@Consumes({ "multipart/form-data" })
-	@Produces("image/png")
+	@Produces({ "image/png", "application/json" })
 	@ApiOperation(value = "Make a prediction image for the given molecule", 
 	notes = "Predict a given molecule to get a prediction image, accepts SMILES, MDL v2000 or MDL v3000 format. In case a MDL is sent, it must be properly URL-encoded in UTF-8. You can use for instance https://www.urlencoder.org/ to encode your file.",
 	response = Void.class, 
@@ -100,21 +104,30 @@ public class PredictApi  {
 
 			@ApiResponse(code = 503, message = "Service not available", response = Error.class) })
 	public Response predictImageGet( 
-			@ApiParam(value = "(Depricated) Compound structure notation using SMILES notation", required=false)
+
+			@ApiParam(value = "Compound structure notation using SMILES or MDL format", 
+			required=false, 
+			defaultValue="CCCCC=O")
+			@QueryParam("molecule") String molecule,
+
+			@ApiParam(value = "**(Depricated)** Compound structure notation using SMILES notation", required=false)
 			@QueryParam("smiles") String smiles,
-			@ApiParam(value = "Compound structure notation using SMILES or MDL format", required=false)
-			@DefaultValue("CCCCC=O") @QueryParam("molecule") String molecule,
+
 			@ApiParam(value = "Image width (min 50 pixels, max 5000 pixels)", allowableValues="range[50,5000]")
 			@DefaultValue("600") @QueryParam("imageWidth") int imageWidth,
+			
 			@ApiParam(value = "Image height (min 50 pixels, max 5000 pixels)", allowableValues="range[50,5000]")
 			@DefaultValue("600") @QueryParam("imageHeight") int imageHeight,
-			@ApiParam(value = "Confidence of prediction (writes interval in figure)", required=false)
+			
+			@ApiParam(value = "Confidence of prediction (writes prediction interval in figure)", required=false)
 			@QueryParam("confidence") Double confidence,
+			
 			@ApiParam(value = "Add title to the image (using the model name)")
 			@DefaultValue("false") @QueryParam("addTitle") boolean addTitle,
+			
 			@Context SecurityContext securityContext ) {
 		logger.debug("Initial image-size at API-level: imageHeight="+imageHeight+", imageWidth="+imageWidth);
-		
+
 		if (smiles!=null && !smiles.isEmpty()) // TODO - remove in newer versions!
 			return delegate.predictImageGet(smiles, imageWidth, imageHeight, confidence, addTitle, securityContext);
 		else
