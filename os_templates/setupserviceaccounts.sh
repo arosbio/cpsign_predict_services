1b2874cb85db21c44ed15f630775a6d4893f2dd6
Staffan Arvidsson <staffan.arvidsson@gmail.com>
2023-03-16 14:16:19 +0100

    remove old OS templates - deprecated system anyways

--- os_templates/setupserviceaccounts.sh
+++
@@ -1,6 +0,0 @@
-oc policy add-role-to-user edit system:serviceaccount:modelingweb:default -n modelingweb-deployments
-
-TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-oc login --token=$TOKEN
-
-
